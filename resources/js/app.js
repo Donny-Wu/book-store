20e0f634dc0dc83e88fbfe5d11ef71614185e6de
DonnyWu <donnyw@cnct.co>
2024-10-27 16:28:33 +0800

    新增sweetalert套件

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 import './bootstrap';
 import jQuery from 'jquery';
 window.$ = jQuery;
+import swal from 'sweetalert2';
+window.Swal = swal;
 import.meta.glob([
     '../images/**',
     '../fonts/**',
