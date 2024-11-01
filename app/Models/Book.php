1739b1e1f0d9b7022f905752c9902add1689b374
DonnyWu <donnyw@cnct.co>
2024-11-01 16:13:24 +0800

    建立資料關聯

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,10 @@ class Book extends Model
 {
     //
     protected $guarded = [];
+    public function publisher(){
+        $this->hasOne(Publisher::class);
+    }
+    public function language(){
+        $this->hasOne(Language::class);
+    }
 }
