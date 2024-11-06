a430d391c9c47691d33cf133a29543835872dc8c
DonnyWu <donnyw@cnct.co>
2024-11-06 14:51:22 +0800

    書籍作者多對多建置

---
+++ database/migrations/2024_11_06_064549_create_book_author_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Book;
+use App\Models\Author;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book_author', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignIdFor(Book::class, 'book_id')->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Author::class, 'author_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('book_author');
+    }
+};
