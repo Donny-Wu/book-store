7ebfceb3f56519db6185dc6b16db5a93b7c0738d
DonnyWu <donnyw@cnct.co>
2024-11-06 13:49:47 +0800

    定義角色類型

--- app/Models/BookMaker.php
+++ app/Models/BookMaker.php
@@ -10,4 +10,8 @@ class BookMaker extends Model
     protected $guarded = [];
     /** @use HasFactory<\Database\Factories\BookMakerFactory> */
     use HasFactory;
+    const ROLE_AUTHOR       = 1;//作者
+    const ROLE_EDITOR       = 2;//编辑
+    const ROLE_TRANSLATOR   = 3;//譯者
+    const ROLE_ILLUSTRATOR  = 4;//繪者
 }
