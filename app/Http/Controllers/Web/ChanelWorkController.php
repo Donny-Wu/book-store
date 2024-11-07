605710e8e24b1be9df94aec88fc695105ac377f8
DonnyWu <donnyw@cnct.co>
2024-11-07 16:11:30 +0800

    通路訂單上傳處理調整

--- app/Http/Controllers/Web/ChanelWorkController.php
+++ app/Http/Controllers/Web/ChanelWorkController.php
@@ -5,18 +5,47 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\UploadChanelRequest;
+use App\Interface\ChanelInterface;
+use Illuminate\Support\Str;
 
 class ChanelWorkController extends Controller
 {
     //@dd(request()->session()->previousUrl(),$momo_action)
-    public function __construct(){}
+    private $service;
+    public function __construct(ChanelInterface $service){
+        $this->service = $service;
+    }
     public function upload(UploadChanelRequest $request){
-        $path = $request->file->store('chanels/'.$request->route('service'));
+        // dd($request->file->getClientOriginalExtension(),Str::uuid()->toString());
+        $extension = $request->file->getClientOriginalExtension();
+        $path = $request->file->storeAs('chanels/'.$request->route('service'),Str::uuid()->toString().'.'.$extension);
+        // 讀取 檔案/資料
+        $this->service->fetchData();
+        // 處理 資料邏輯
+        $this->service->processData();
+        // 寫入 資料庫
+        $this->service->writeData();
         $response = [
                 'icon'  => 'success',
                 'title' => '訂單資料上傳成功',
                 'text'  => '訂單資料上傳成功',
         ];
         return redirect()->back()->with('response', $response);
+    }
+    public function fetchAPI(){
+        // // 讀取 檔案/資料
+        // $this->service->fetchData();
+        // // 處理 資料邏輯
+        // $this->service->processData();
+        // // 寫入 資料庫
+        // $this->service->writeData();
+    }
+    public function fetchERP(){
+        // // 讀取 檔案/資料
+        // $this->service->fetchData();
+        // // 處理 資料邏輯
+        // $this->service->processData();
+        // // 寫入 資料庫
+        // $this->service->writeData();
     }
 }
