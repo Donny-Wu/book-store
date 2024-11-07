5edb2039120a4d2bd17d8e5da2876a63904bbbfa
DonnyWu <donnyw@cnct.co>
2024-11-07 16:12:32 +0800

    抽象定義說明

--- app/Interface/ChanelInterface.php
+++ app/Interface/ChanelInterface.php
@@ -4,8 +4,31 @@ namespace App\Interface;
 
 interface ChanelInterface
 {
-    //
+    /**
+     * Summary of fetchData
+     * 讀取資料
+     * 1.檔案
+     * 2.API
+     * 3.資料庫(ERP)
+     * @return void
+     */
     public function fetchData();
+    /**
+     * Summary of processData
+     * 處理資料
+     * 1.驗證
+     * 2.格式轉換
+     * 3.計算邏輯
+     * @return void
+     */
     public function processData();
+    /**
+     * Summary of writeData
+     * 寫入資料
+     * 1.檔案
+     * 2.資料庫(MySQL、ERP)
+     *
+     * @return void
+     */
     public function writeData();
 }
