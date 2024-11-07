c4e2cf3829e3f5f1496f69bd30d0ea0c509c217e
DonnyWu <donnyw@cnct.co>
2024-11-07 18:00:41 +0800

    新增誠品資料匯入

---
+++ app/Imports/ChanelEliteImport.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Imports;
+
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+
+class ChanelEliteImport implements ToCollection
+{
+    private $collection;
+    /**
+    * @param Collection $collection
+    */
+    public function collection(Collection $collection)
+    {
+        //
+        $this->collection = $collection;
+    }
+    public function get(){
+        return $this->collection;
+    }
+}
