d4fd089800678c8a70d95fa7fd9c2b813d469f34
DonnyWu <donnyw@cnct.co>
2024-11-07 11:52:38 +0800

    增修通路訂單驗證功能

---
+++ app/Http/Requests/UploadChanelRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Enum\Chanel;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Contracts\Validation\Validator;
+
+class UploadChanelRequest extends FormRequest
+{
+    private $momo_rules = [
+        'file'         => ['required', 'mimes:csv', 'max:10000'],
+    ];
+    private $elite_rules = [
+        'file'         => ['required', 'mimes:xls', 'max:10000'],
+    ];
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return match($this->route('service')) {
+            Chanel::MOMO->value  => $this->momo_rules,
+            Chanel::ELITE->value => $this->elite_rules
+        };
+    }
+    protected function failedValidation(Validator $validator){
+        // dd($validator->errorBag());
+        $validator->errors()->add('service',$this->route('service'));
+        throw (new ValidationException($validator));
+    }
+}
